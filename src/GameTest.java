fd984a16c2d8b883c1b71c5723e1a99e83972ed5
TobiasTheDanish <t.hc199811@gmail.com>
2023-02-25 22:57:39 +0100

    Bug fix with scrolling background and minor changes

--- src/GameTest.java
+++ src/GameTest.java
@@ -6,33 +6,29 @@ import JGE.Game;
 import Math.Vector.Vector2D;
 import Math.Vector.Vector3D;
 import JGE.GameObjects.ScrollingBackground;
-import Utils.Input.Input;
-
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_P;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_R;
 
 public class GameTest extends Game {
     GameWorld world;
 
     ScrollingBackground[] bg;
-    private boolean gamePaused = true;
 
     @Override
     protected void init() {
         world = new GameWorld2D(windowWidth, windowHeight);
 
+        Bird bird = new Bird(new Transform(new Vector3D(windowWidth/3, windowHeight/2), new Vector2D(100.0f, 100.0f)), "./res/textures/flappy_bird.png");
+
         bg = new ScrollingBackground[5];
-        bg[0] = new ScrollingBackground(new Transform( new Vector3D(windowWidth * 0.165f, windowHeight / 2, 0.0f), new Vector3D(), new Vector2D(windowWidth/3, windowHeight), 1.0f), "./res/textures/flappy_bg.jpeg");
-        bg[1] = new ScrollingBackground(new Transform( new Vector3D(windowWidth * 0.495f, windowHeight / 2, 0.0f), new Vector3D(), new Vector2D(windowWidth/3, windowHeight), 1.0f), "./res/textures/flappy_bg.jpeg");
-        bg[2] = new ScrollingBackground(new Transform( new Vector3D(windowWidth * 0.825f, windowHeight / 2, 0.0f), new Vector3D(), new Vector2D(windowWidth/3, windowHeight), 1.0f), "./res/textures/flappy_bg.jpeg");
-        bg[3] = new ScrollingBackground(new Transform( new Vector3D(windowWidth * 1.155f, windowHeight / 2, 0.0f), new Vector3D(), new Vector2D(windowWidth/3, windowHeight), 1.0f), "./res/textures/flappy_bg.jpeg");
-        bg[4] = new ScrollingBackground(new Transform( new Vector3D(windowWidth * 1.485f, windowHeight / 2, 0.0f), new Vector3D(), new Vector2D(windowWidth/3, windowHeight), 1.0f), "./res/textures/flappy_bg.jpeg");
+        bg[0] = new ScrollingBackground(2.0f, new Transform( new Vector3D(windowWidth * 0.0f, windowHeight / 2), new Vector2D(windowWidth/3, windowHeight)), "./res/textures/flappy_bg.jpeg");
+        bg[1] = new ScrollingBackground(2.0f, new Transform( new Vector3D(windowWidth * 0.33f, windowHeight / 2), new Vector2D(windowWidth/3, windowHeight)), "./res/textures/flappy_bg.jpeg");
+        bg[2] = new ScrollingBackground(2.0f, new Transform( new Vector3D(windowWidth * 0.66f, windowHeight / 2), new Vector2D(windowWidth/3, windowHeight)), "./res/textures/flappy_bg.jpeg");
+        bg[3] = new ScrollingBackground(2.0f, new Transform( new Vector3D(windowWidth * 0.99f, windowHeight / 2), new Vector2D(windowWidth/3, windowHeight)), "./res/textures/flappy_bg.jpeg");
+        bg[4] = new ScrollingBackground(2.0f, new Transform( new Vector3D(windowWidth * 1.32f, windowHeight / 2), new Vector2D(windowWidth/3, windowHeight)), "./res/textures/flappy_bg.jpeg");
 
         for (ScrollingBackground sbg : bg) {
             world.addGameObject(sbg);
         }
 
-        Bird bird = new Bird(new Transform(new Vector3D(windowWidth/3, windowHeight/2,-0.9f), new Vector2D(100.0f, 100.0f)), "./res/textures/flappy_bird.png");
         world.addGameObject(bird);
     }
 
@@ -40,17 +36,7 @@ public class GameTest extends Game {
     protected void update() {
         super.update();
 
-        if (Input.keys[GLFW_KEY_P]) {
-            gamePaused = true;
-        }
-
-        if (Input.keys[GLFW_KEY_R]) {
-            gamePaused = false;
-        }
-
-        if (!gamePaused) {
-            world.update();
-        }
+        world.update();
     }
 
     @Override
--- src/JGE/GameObjects/ScrollingBackground.java
+++ src/JGE/GameObjects/ScrollingBackground.java
@@ -1,29 +1,32 @@
 package JGE.GameObjects;
 
-import Math.Matrix.Matrix4;
 import Math.Vector.Vector2D;
 import Math.Vector.Vector3D;
 import JGE.GameComponents.*;
 import Shapes.Primitives.Quad;
 
 public class ScrollingBackground extends GameObject {
-    public ScrollingBackground(Transform transform, String texturePath) {
+    private float speed;
+    private int frameCounter=0;
+    public ScrollingBackground(float speed, Transform transform, String texturePath) {
         this.transform = transform;
+        this.speed = speed;
 
         shape = new Quad(new Vector2D(this.transform.dimensions.x, this.transform.dimensions.y), texturePath);
     }
 
     public void update() {
-        transform.position.add(Vector3D.left.multiply(2.0f));
+        if (frameCounter >= 120 && speed < 6.5f) {
+            frameCounter -= 120;
+            speed *= 1.05f;
+        }
+
+        transform.position.add(Vector3D.left.multiply(speed));
 
         if (transform.position.x < 0 - transform.dimensions.x/2) {
-            transform.position.x = this.transform.dimensions.x * 3f;
+            transform.position.x = this.transform.dimensions.x * 4.445f;
         }
-    }
 
-    public void render() {
-        for (int i = 0; i < 3; i++) {
-            shape.render();
-        }
+        frameCounter++;
     }
 }
